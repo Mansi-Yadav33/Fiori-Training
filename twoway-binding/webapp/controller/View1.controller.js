3f3f5454599fb29a439ad07364622066be75bebc
Mansi Yadav <ymansi004@gmail.com>
2024-04-09 10:10:20 +0000

    new changes

--- twoway-binding/webapp/controller/View1.controller.js
+++ twoway-binding/webapp/controller/View1.controller.js
@@ -1,16 +1,19 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, MessageToast) {
         "use strict";
 
         return Controller.extend("twowaybinding.controller.View1", {
             onInit: function () {
                 var oNewModel = new JSONModel();
+                MessageToast.show("Default Binding Mode is: " + oNewModel.getDefaultBindingMode());
                 this.getView().setModel(oNewModel, "oNewModel");
                 oNewModel.setProperty("/FirstName", "Fiori Training");
             },
@@ -18,12 +21,7 @@ sap.ui.define([
             onButtonPress: function (oEvent) {
                 var oNewModel = this.getView().getModel("oNewModel");
                 oNewModel.setProperty("/FirstName", "Two way binding");
-            },
-            onChange: function (oEvent) {
-                var name = oEvent.getParameter("/FirstName");
-                this.getView().getModel("oNewModel").setProperty("/FirstName", name);
-                
             }
+           
         });
     });
-
